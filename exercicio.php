ccabe2848702b9ea5afe67e4c3fd51fd16ca7542
Jeronimo, Aleixo A <jeronimo86@users.noreply.github.com>
2017-09-06 22:06:24 +0100

    Solucao do Exercicio #2

---
+++ exercicio.php
@@ -0,0 +1,21 @@
+<?php
+
+define (meu_nome, "AjA", false);
+$dia_nascimento = 29;
+$mes_nascimento = "Setembro";
+$ano_nascimento = 1980;
+$ano_actual = 2017;
+
+function idade_usuario() {
+    global $ano_actual;
+    global $ano_nascimento;
+    
+    echo $ano_actual - $ano_nascimento;
+}
+
+idade_usuario();
+echo meu_nome;
+
+
+
+?>
