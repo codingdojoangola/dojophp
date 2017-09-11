9fcb6346c223a48859d00f84b7116132c9103243
Eleandro Duzentos <eleandro@inbox.ru>
2017-09-11 23:40:06 +0100

    E200 (#6)
    * Movido da pasta src para src/CalculadorasDeIdade
    Namespaces actualizados para corresponder a nova localização
    
    * Foram movidos
    
    * Actualizado namespace da classe em teste
    
    * Adicionado teste automatizado para classe ResolveIdade
    
    * Adicionada pasta .idea
    
    * Adicionada anotação de autor
    Alterado o tipo de retorno
    
    * Adionada anotações de autor
    
    * Movido testes para pasta Calculadoras
    
    * Apply fixes from StyleCI
    
    * Resolvido o problema dos arquivos anteriores

--- src/CalculadorasDeIdade/MyCalculadora.php
+++ src/CalculadorasDeIdade/MyCalculadora.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DojoPHP;
+namespace DojoPHP\CalculadorasDeIdade;
 
 class MyCalculadora
 {
---
+++ src/CalculadorasDeIdade/ResolveIdades.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DojoPHP\CalculadorasDeIdade;
+
+use DateTime;
+
+/**
+ * Class ResolveIdades.
+ *
+ * Calcula idades
+ *
+ * Baseado em: http://www.phpit.com.br/artigos/descobrindo-a-idade-atraves-da-data-de-nascimento.phpit
+ *
+ * Eu não conhecia a função floor(),
+ * mas sabia que a função ceil() arredondava números
+ * decimais, então substitui o floor() do código
+ * de origem pela ceil(), mas quando testei...
+ *
+ * Boom! Falhou... :)
+ *
+ * Interessante essa função floor().
+ *
+ * @see www.hackingwithphp.com/4/6/1/rounding
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru|github:e200>
+ */
+class ResolveIdades
+{
+    /**
+     * Calcula usando uma data.
+     *
+     * @param $dataDeNascimento
+     *
+     * @return int
+     */
+    public function comData($dataDeNascimento)
+    {
+        $tempoUnix = (new DateTime($dataDeNascimento))->getTimestamp();
+        $tempoAgora = time();
+
+        return floor(((((($tempoAgora - $tempoUnix) / 60) / 60) / 24) / 365.25));
+    }
+}
