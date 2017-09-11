3d6d3eb30a5f3bf781863974bbc6d7f9a8454ea8
Marcio Zebedeu <marciozebedeu@gmail.com>
2017-09-11 06:46:39 +0100

    fix

--- src/API.php
+++ src/API.php
@@ -8,29 +8,30 @@ class API implements interfaceApi
 {
 
 
-   public function get($argm) {
-     $ch = curl_init();
- 
-    curl_setopt($ch, CURLOPT_URL, $argm);
-     curl_setopt($ch, CURLOPT_REFERER, "http://www.example.org/yay.htm");
-    curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $output = curl_exec($ch);
-    curl_close($ch);
- $decoded = json_decode($output);
+    public function get($argm)
+    {
+        $ch = curl_init();
 
-if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-    die('error occured: ' . $decoded->response->errormessage);
-}
-if(json_last_error() === JSON_ERROR_NONE){
-    //if not joson, is xml'
-    return(($decoded));
+        curl_setopt($ch, CURLOPT_URL, $argm);
+        curl_setopt($ch, CURLOPT_REFERER, "http://www.example.org/yay.htm");
+        curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $decoded = json_decode($output);
 
-}else{
-    // if json true, return json
-    return(($output));
+        if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+            die('error occured: ' . $decoded->response->errormessage);
+        }
+        if (json_last_error() === JSON_ERROR_NONE) {
+            //if not joson, is xml'
+            return (($decoded));
 
-      }
-}
+        } else {
+            // if json true, return json
+            return (($output));
+
+        }
+    }
 }
