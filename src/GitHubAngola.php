824a1c25d058fc82f2bf748817ce7bcf1b0f15cb
guimar86 <guillenmartins@gmail.com>
2017-09-17 02:05:32 +0100

    Challenge partially completed. Need phpUnit Test

--- src/GitHubAngola.php
+++ src/GitHubAngola.php
@@ -2,79 +2,48 @@
 
 /**
  * User: Renato Martins
- * Date: 12/09/2017
- * Time: 21:13
+ * Date: 16/09/2017
+ * Time: 20:13
  */
 class GitHubAngola
 {
 
 
-
-    function getCurlVal(){
-
-        $curl_init=curl_init();
-        $url="curl -u guimar86:540bdd5a9a1d676d7e4ca4b082aaf62d12fcc8ee https://api.github.com/search/users?q=location:Luanda/Angola";
-        curl_setopt($curl_init,CURLOPT_URL,$url);
-        curl_setopt($curl_init,CURLOPT_RETURNTRANSFER,$url);
-
-        $result=curl_exec($curl_init);
-
-
-    }
     /**
-     * Função usada para ler o endpoint do gitHub
-     * e obter todos utilizadores em Angola
-     * =====AUTENTICACAO EM FALTA====
-     * @return array
+     * Metodo usado para retornar a lista de utilizadores pela
+     * localizacao indicada
+     * @return mixed
      */
-    function getGitHubAngolanUsers()
+    function getGitHubUsersByLocation($location)
     {
 
-        $toReturn = array();
-        //$github_url=curl -u guimar86:540bdd5a9a1d676d7e4ca4b082aaf62d12fcc8ee https://api.github.com/search/users?q=location:Luanda/Angola
-        $github_url = 'https://api.github.com/search/users?q=location:Luanda/Angola';
-        $github_json = file_get_contents($github_url);
-        $github_array = json_decode($github_json, true);
-        
+        if (!isset($location)) {
 
-        if (!empty($github_array)) {
-
-            foreach ($github_array['data'] as $names) {
-
-                array_push($toReturn, ['total_count']['incomplete_results']['items']['login']);
-            }
-
+            trigger_error("Falta o parametro localizacao", E_USER_ERROR);
         }
 
-        return $toReturn;
+
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+        $ch = curl_init();
+        $url = "https://api.github.com/search/users?q=location:" . $location;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        $gitReturn = json_decode($result, true);
+
+        //var_dump($gitReturn);
+
+        return $gitReturn;
+
     }
 
 
-    /**
-     * Função usada para ler o endpoint do rest api do github
-     * e devolver a lista de utilizadores na localizcao solicitada.
-     * ==== AUTENTICACAO EM FALTA ====
-     * @param $location
-     * @return array
-     */
-    function getGitHubUserByLocation($location)
-    {
-
-        $toReturn = array();
-        $github_url = 'https://api.github.com/search/users?q=location:' . $location;
-        $github_json = file_get_contents($github_url);
-        $github_array = json_decode($github_json, true);
-
-        if (!empty($github_array)) {
-
-            foreach ($github_array['data'] as $names) {
-
-                array_push($toReturn, ['total_count']['incomplete_results']['items']['login']);
-            }
-
-        }
-
-        return $toReturn;
-    }
 
 }
