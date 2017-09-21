a33c14de7fd932d11bd312a40172e0012b14b124
Acidiney Dias <acidineydias@gmail.com>
2017-09-21 16:00:05 +0100

    Commit Initial

---
+++ tests/GitHub/githubTest.php
@@ -0,0 +1,20 @@
+<?php
+	
+	use dojophp\returnUsersGithub\githubApi;
+	use PHPUnit\Framework\TestCase as PHPUnit;
+
+class githubTest extends PHPUnit
+{
+
+	public $search;
+	public $git1;
+
+	public function test()
+	{
+		$this->search 	= "Luanda";
+		$this->git1		= new githubApi($this->search);
+
+
+		return $this->git1->get_users();
+	}
+}
