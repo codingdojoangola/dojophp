5b3a3a95b2aaa3450a6378ca89b6f79faef5a7f3
Acidiney Dias <acidineydias@gmail.com>
2017-09-22 10:11:24 +0100

    Update githubTest.php

--- tests/GitHub/githubTest.php
+++ tests/GitHub/githubTest.php
@@ -3,18 +3,18 @@
 	use dojophp\returnUsersGithub\githubApi;
 	use PHPUnit\Framework\TestCase as PHPUnit;
 
-	class githubTest extends PHPUnit
+class githubTest extends PHPUnit
+{
+
+	public $search;
+	public $git1;
+
+	public function test()
 	{
+		$this->search 	= "Luanda";
+		$this->git1		= githubApi::get_users($this->search);
 
-		public $search;
-		public $git1;
 
-		public function test()
-		{
-			$this->search 	= "Luanda";
-			$this->git1	= new githubApi($this->search);
-
-			return $this->git1->get_users();
-		}
+		return $this->git1;
 	}
-?>
+}
