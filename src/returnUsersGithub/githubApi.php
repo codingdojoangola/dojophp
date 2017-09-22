7c1ea1f5f9234aadc9c4f94d0ff8801f8d4c31eb
Acidiney Dias <acidineydias@gmail.com>
2017-09-22 11:39:29 +0100

    Update githubApi.php

--- src/returnUsersGithub/githubApi.php
+++ src/returnUsersGithub/githubApi.php
@@ -20,10 +20,10 @@
 
 		public function __construct()
 		{
-			$this->userAgent = $_SERVER['HTTP_USER_AGENT'];
-			$this->return_json = array();
-			$this->url    	   = 'https://api.github.com/search/users?q=location:';
-			$this->ch 		   = curl_init();
+			$this->userAgent 	= $_SERVER['HTTP_USER_AGENT'];
+			$this->return_json 	= array();
+			$this->url    	   	= 'https://api.github.com/search/users?q=location:';
+			$this->ch		= curl_init();
 		}
 
 		public function get_users($location)
@@ -32,15 +32,15 @@
 				$this->location = $location;
 				/*Este bloco de codigo verifica se existe Angola no parametro, pra não haver redundância*/
 				if(strpos($this->location, "Angola")): 	$this->url = $this->url.$this->location."/Angola";
-				else:									$this->url = $this->url.$this->location;
+				else:					$this->url = $this->url.$this->location;
 				endif;
 
 				curl_setopt($this->ch, CURLOPT_URL, $this->url);	
 				curl_setopt($this->ch, CURLOPT_USERAGENT, $this->userAgent);
-        		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);		
+        			curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);		
 				curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-
 				$this->users_json = curl_exec($this->ch);
+				
 				curl_close($this->ch);
 				return json_decode($this->users_json);
 		 	} catch (Exception $e) 
