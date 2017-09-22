1f44eb8e9e1776764b28b4d314da27d1d6d10216
Acidiney Dias <acidineydias@gmail.com>
2017-09-22 10:09:20 +0100

    Update githubApi.php

--- src/returnUsersGithub/githubApi.php
+++ src/returnUsersGithub/githubApi.php
@@ -18,14 +18,18 @@
 		Public $url;
 		public $userAgent;
 
-		public function __construct($location)
+		public function __construct()
 		{
 			$this->userAgent = $_SERVER['HTTP_USER_AGENT'];
-			$this->location = $location;
 			$this->return_json = array();
 			$this->url    	   = 'https://api.github.com/search/users?q=location:';
 			$this->ch 		   = curl_init();
+		}
 
+		public function get_users($location)
+		{
+			try{
+				$this->location = $location;
 				/*Este bloco de codigo verifica se existe Angola no parametro, pra não haver redundância*/
 				if(strpos($this->location, "Angola")): 	$this->url = $this->url.$this->location."/Angola";
 				else:									$this->url = $this->url.$this->location;
@@ -34,15 +38,10 @@
 				curl_setopt($this->ch, CURLOPT_URL, $this->url);	
 				curl_setopt($this->ch, CURLOPT_USERAGENT, $this->userAgent);
         		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);		
-				curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);			
+				curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+
 				$this->users_json = curl_exec($this->ch);
 				curl_close($this->ch);
-		}
-
-		public function get_users()
-		{
-			try{
-
 				return json_decode($this->users_json);
 		 	} catch (Exception $e) 
 		 	{
