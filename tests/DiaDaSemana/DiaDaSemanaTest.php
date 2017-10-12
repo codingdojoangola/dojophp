d9edce34e4e6fd8182d9525c77c3d279466183a5
Acidiney Dias <neyboyruandias@gmail.com>
2017-10-13 00:10:38 +0200

    sei lá

---
+++ tests/DiaDaSemana/DiaDaSemanaTest.php
@@ -0,0 +1,29 @@
+<?php
+
+	use DojoPHP\DiasDaSemana\DiasDaSemanaAPI;
+	use PHPUnit\Framework\TestCase;
+
+	/**
+	 *@author: Acidiney Dias
+	 *@link: https://www.github.com/acidiney
+	 *@description: Classe de testes para o retorno do DiasDaSemanaAPI
+	 */
+	class DiaDaSemanaTest extends TestCase
+	{
+		public $data;
+		public $instancia;
+		public $diaPorExtenso;
+
+		public function testRetornaDoMetodo()
+		{
+			$this->data = "12-07-2017";
+
+			$this->instancia = new DiasDaSemanaAPI($this->data);
+			$this->diaPorExtenso = $this->instancia->RetornaDiaDaSemana();
+
+			//Verificando se o retorno não é vazio(nulo) e se está retornando uma string --> PHPUnit
+			$this->assertNotEmpty($this->diaPorExtenso);
+
+			return $this->diaPorExtenso;
+		}
+	}
