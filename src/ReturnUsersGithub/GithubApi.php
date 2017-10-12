619586a4017e37eb7f986a2938c37eb46de07ae3
Acidiney Dias <acidineydias@gmail.com>
2017-10-12 01:06:15 +0100

    StyleCI

--- src/ReturnUsersGithub/GithubApi.php
+++ src/ReturnUsersGithub/GithubApi.php
@@ -3,16 +3,15 @@
 namespace DojoPHP\ReturnUsersGithub;
   
 /**
- * @author: Acidiney Dias
- * @description: Return users residing in Angola
- * @param: $location
- * @git: CodingDojo/dojophp
- * @return: json
- * @method: get
- */
+  * @author: Acidiney Dias
+  * @description: Return users residing in Angola
+  * @param: $location
+  * @git: CodingDojo/dojophp
+  * @return: json
+  * @method: get
+  */
 class GithubApi
 {
-
     public $location;
     public $users_json;
     public $return_json;
@@ -28,26 +27,27 @@ class GithubApi
         $this->url = 'https://api.github.com/search/users?q=location:';
         $this->ch = curl_init();
     }
-  
+
     public function get_users()
     {
         try {
-  
+
             /*Este bloco de codigo verifica se existe Angola no parametro, pra não haver redundância*/
             if (strpos($this->location, 'Angola')) :
-                $this->url = $this->url.$this->location.'/Angola'; else:
+                $this->url = $this->url.$this->location.'/Angola'; 
+            else:
                     $this->url = $this->url.$this->location;
-                endif;
+            endif;
   
 
-                curl_setopt($this->ch, CURLOPT_URL, $this->url);
-                curl_setopt($this->ch, CURLOPT_USERAGENT, $this->userAgent);
-                curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-                $this->users_json = curl_exec($this->ch);
+            curl_setopt($this->ch, CURLOPT_URL, $this->url);
+            curl_setopt($this->ch, CURLOPT_USERAGENT, $this->userAgent);
+            curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+            $this->users_json = curl_exec($this->ch);
   
 
-                return json_decode($this->users_json, true);
+            return json_decode($this->users_json, true);
         } catch (Exception $e) {
             echo json_encode($e);
         }
--- tests/GitHub/GithubTest.php
+++ tests/GitHub/GithubTest.php
@@ -1,19 +1,19 @@
-<?php   
+<?php  
 
  use DojoPHP\ReturnUsersGithub\GithubApi;
  use PHPUnit\Framework\TestCase;
-  
+
  class GithubTest extends TestCase
  {
-    public $search;
-    public $git1;
+     public $search;
+     public $git1;
   
-    public function testGithubUserAngola()
-    {
-        $this->search = 'Luanda';
-        $this->git1 = new GithubApi($this->search);
-        $this->assertNotEmpty($this->git1->get_users());
-        $this->assertInternalType('array', $this->git1->get_users());
-        $this->assertArrayHasKey('items', $this->git1->get_users());
-    }
- }
+     public function testGithubUserAngola()
+     {
+         $this->search = 'Luanda';
+         $this->git1 = new GithubApi($this->search);
+         $this->assertNotEmpty($this->git1->get_users());
+         $this->assertInternalType('array', $this->git1->get_users());
+         $this->assertArrayHasKey('items', $this->git1->get_users());
+     }
+  }
