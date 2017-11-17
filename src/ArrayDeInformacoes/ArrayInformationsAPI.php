7ed697d8dae9a4500d437116e6302669920264e4
Acidiney Dias <acidineydias@gmail.com>
2017-11-17 11:56:32 +0100

    styleCI

--- src/ArrayDeInformacoes/ArrayInformationsAPI.php
+++ src/ArrayDeInformacoes/ArrayInformationsAPI.php
@@ -25,28 +25,12 @@ class ArrayInformationsAPI
 
     public function CreateArrayWithInformation()
     {
-<<<<<<< HEAD
         $this->data =
           [
-<<<<<<< HEAD
             'E-mail'    => $this->email,
             'Name'      => $this->name,
             'Telefone'  => $this->telephone,
           ];
-=======
-            'E-mail'   => $this->email,
-            'Name'     => $this->name,
-            'Telefone' => $this->telephone,
-         ];
->>>>>>> 6f5974ba3bdf6b90be4d5f374a80f1ab78d833e2
-=======
-        $this->data = 
-          [
-            'E-mail' => $this->email,
-            'Name' => $this->name,
-            'Telefone' => $this->telephone, 
-         ];
->>>>>>> 121482478c788d821744c6ff358ef8c988eef12c
 
         return $this->data;
     }
